a917889c61d3eca4e28dd97b9887e2812fee7d8d
dain2116 <dain2116@gmail.com>
2020-07-01 01:37:41 +0900

    Memo

--- app/src/main/java/com/example/memoproject/MainActivity.java
+++ app/src/main/java/com/example/memoproject/MainActivity.java
@@ -24,5 +24,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadButton = (Button) findViewById(R.id.load);
+        saveButton = (Button) findViewById(R.id.save);
+        deleteButton = (Button) findViewById(R.id.delete);
+        inputText = (EditText) findViewById(R.id.inputText);
+
+        loadButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                FileInputStream fis = null;
+                try{
+                    fis = openFileInput("memo.txt");
+                    byte[] data = new byte[fis.available()];
+                    while( fis.read(data) != -1){
+                    }
+                    inputText.setText(new String(data));
+                    Toast.makeText(getApplicationContext(), "load completed", Toast.LENGTH_SHORT).show();
+                }catch(Exception e){
+                    e.printStackTrace();
+                }finally{
+                    try{ if(fis != null) fis.close(); }catch(Exception e){e.printStackTrace();}
+                }
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                FileOutputStream fos = null;
+                try{
+                    fos = openFileOutput("memo.txt", MODE_PRIVATE);
+                    String out = inputText.getText().toString();
+                    fos.write(out.getBytes());
+                    Toast.makeText(getApplicationContext(), "save completed", Toast.LENGTH_SHORT).show();
+                }catch(Exception e){
+                    e.printStackTrace();
+                }finally{
+                    try{ if(fos != null) fos.close(); }catch(Exception e){e.printStackTrace();}
+                }
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                boolean b = deleteFile("memo.txt");
+                if(b){
+                    Toast.makeText(getApplicationContext(), "delete completed", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "delete failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
